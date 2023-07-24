caccf883b5fd92e6ad1681b6b18e04fe3bc2bb3d
pwamly <pwamly2016@gmail.com>
2023-07-24 16:11:34 +0300

    get request

--- Widget.js
+++ Widget.js
@@ -1,27 +1,28 @@
 // Your widget JavaScript file (e.g., UserSettings.js)
 
 define([
-  'dojo/_base/declare',
-  'jimu/BaseWidget',
-  'dojo/dom',
-  'dojo/on',
-  'dojo/dom-construct',
-  './helper', // Import the helper module
-  'dojo/text!./config.json', // Import the configuration file using dojo/text
-  'dojo/domReady!'
+  "dojo/_base/declare",
+  "jimu/BaseWidget",
+  "dojo/dom",
+  "dojo/on",
+  "dojo/dom-construct",
+  "./helper", // Import the helper module
+  "dojo/text!./config.json", // Import the configuration file using dojo/text
+  "dojo/domReady!",
 ], function (declare, BaseWidget, dom, on, domConstruct, helper, config) {
+  // Import the 'fetch' polyfill
+  // require("whatwg-fetch");
 
   // Parse the configuration file as a JSON object
   var configObj = JSON.parse(config);
 
   // To create a widget, you need to derive from BaseWidget.
   return declare([BaseWidget], {
+    baseClass: "jimu-widget-UserSettings",
+    name: "UserSettings",
+    className: "esri.widgets.UserSettings",
 
-    baseClass: 'jimu-widget-UserSettings',
-    name: 'UserSettings',
-    className: 'esri.widgets.UserSettings',
-
-    postCreate: function() {
+    postCreate: function () {
       this.inherited(arguments);
 
       console.log("Hello, World!");
@@ -30,7 +31,7 @@ define([
       this._addStartButton();
     },
 
-    _addStartButton: function() {
+    _addStartButton: function () {
       const wrapperDiv = domConstruct.create(
         "div",
         { class: "wrapper-div" },
@@ -49,33 +50,58 @@ define([
       on(this.startButton, "click", this._onStartButtonClick.bind(this));
     },
 
-    _onStartButtonClick: function() {
+    _onStartButtonClick: function () {
       // Function to be executed when the "Run elaboration" button is clicked
-      var inputValue = dom.byId('inputId').value;
+      var inputValue = dom.byId("inputId").value;
       console.log("Input value:...........", configObj.services[0].apiEndpoint);
 
       // Perform the POST request to the API using the exported function from helper
-      helper.postToApi(configObj.services[0].apiEndpoint, { input: inputValue })
-        .then(function(response) {
+      helper
+        .postToApi(configObj.services[0].apiEndpoint, { input: inputValue })
+        .then(function (response) {
           // Success callback
-          console.log('POST request successful:', response);
+          console.log("POST request successful:", response);
         })
-        .catch(function(error) {
+        .catch(function (error) {
           // Error callback
-          console.error('POST request failed:', );
+          console.error("POST request failed:");
         });
     },
 
-    startup: function() {
-      this.inherited(arguments);
-
-      // Your widget startup code goes here
+    performGetRequest: function () {
+      // Use the fetch API to perform a GET request
+      fetch(configObj.services[0].apiEndpoint)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Process the data received from the server
+          console.log("Data received:", data);
+        })
+        .catch((error) => {
+          // Handle any errors that occurred during the request
+          console.error("Error during GET request:", error);
+        });
     },
 
-    onClose: function() {
+    startup: function () {
+      this.inherited(arguments);
+      // Your widget startup code goes here
+
+      // Perform a GET request when the widget is rendered
+    },
+
+    onOpen: function(){
+      this.performGetRequest();
+    },
+
+    onClose: function () {
       // Your widget cleanup code goes here
 
       this.inherited(arguments);
-    }
+    },
   });
 });
